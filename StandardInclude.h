f4210f9bdbc03f3e5a7c1b605c578fa5bac2b5f7
Leen Al-Ahmad <alahmadl@sheridancollege.ca>
2024-09-18 09:38:25 -0500

    GLM vectors added in StandardInclude.h

--- StandardInclude.h
+++ StandardInclude.h
@@ -3,9 +3,12 @@
 #ifndef STANDARD_INCLUDE_H
 #define STANDARD_INCLUDE_H
 
+#define GLM_ENABLE_EXPERIMANTAL
+
 #include <vector>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 #ifdef  _WIN32
 #include <Windows.h>
@@ -16,7 +19,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include  <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include "Singleton.h"
+
+using namespace std;
 
 #endif // !STANDARD_INCLUDE_H
